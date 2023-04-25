d33fa7e48c372b11cabdaa6fc42b64432853864f
David Xu <david.xufin@gmail.com>
2023-04-25 11:45:33 +0300

    Created a palindrome detection program in C#

---
+++ DotNet/PalindromeDetectCSharp/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Please enter a string:");
+
+string input = Console.ReadLine();
+
+
+// method 1
+string reversed ="";
+for (int index = input.Length - 1; index >= 0 ; index--)
+{
+    reversed += input[index];
+}
+
+Console.WriteLine(reversed);
+
+// compare the input to the reversed string
+bool isPalindrome = (input == reversed);
+
+if (isPalindrome)
+{
+    Console.WriteLine("The Input is a palindrome");
+}
+else
+{   
+    Console.WriteLine("The input is not a palindrome");
+}
