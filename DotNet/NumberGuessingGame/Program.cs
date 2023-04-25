a284ec63e88c3ee726b0c0d09efc5578d90787d9
David Xu <david.xufin@gmail.com>
2023-04-25 14:44:48 +0300

    Finished the C# random number guessing game

--- DotNet/NumberGuessingGame/Program.cs
+++ DotNet/NumberGuessingGame/Program.cs
@@ -1,10 +1,37 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Welcome to the Number Guessing Game!");
 
-int correct = Random.Shared.Next(1,21);
+// Generates a random number between 1 and 20
+int correct = Random.Shared.Next(1, 21);
 Console.WriteLine(correct);
 
 
-Console.WriteLine("Game Over!");
+//for loop to tracks the number fo guesses
+for (int index = 0; index < 3; index++)
+{
+    //Asks player to guess a number
+    Console.WriteLine("Please enter your guess between 1 and 20:");
+    string guessValue = Console.ReadLine();
+    int guess = int.Parse(guessValue);
+    
+    //checks the players guess with the generated value
+    if (guess == correct)
+    {
+        Console.WriteLine("You guessed the correct value! You win the Game!");
+        break;
+    }
+    else if (guess < correct)
+    {
+        Console.WriteLine("The correct number is larger.");
+        
+    }
+    else
+    {
+        Console.WriteLine("The correct number is smaller.");
+        
+    }
+}
+
+Console.WriteLine("Game ended");
 
 
