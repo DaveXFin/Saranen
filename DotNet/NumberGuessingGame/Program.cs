9dbb4f1cb7e5153a1d0b8000253875606ed1ac5b
David Xu <david.xufin@gmail.com>
2023-04-25 14:20:29 +0300

    Began making a guessing game in C#

---
+++ DotNet/NumberGuessingGame/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Welcome to the Number Guessing Game!");
+
+int correct = Random.Shared.Next(1,21);
+Console.WriteLine(correct);
+
+
+Console.WriteLine("Game Over!");
+
+
