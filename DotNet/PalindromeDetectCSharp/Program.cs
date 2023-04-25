89e287f4cb1bdffaa13493175b70557c582a9d64
David Xu <david.xufin@gmail.com>
2023-04-25 11:54:51 +0300

    Added a seoncd metho to solve the palindorme decteion in C#

--- DotNet/PalindromeDetectCSharp/Program.cs
+++ DotNet/PalindromeDetectCSharp/Program.cs
@@ -5,6 +5,7 @@ string input = Console.ReadLine();
 
 
 // method 1
+/*
 string reversed ="";
 for (int index = input.Length - 1; index >= 0 ; index--)
 {
@@ -12,10 +13,21 @@ for (int index = input.Length - 1; index >= 0 ; index--)
 }
 
 Console.WriteLine(reversed);
+*/
+
+// method 2
+
+char[] chars = input.ToCharArray();
+
+Array.Reverse(chars);
+string reversed = new string(chars);
+Console.WriteLine(reversed);
+
 
 // compare the input to the reversed string
 bool isPalindrome = (input == reversed);
 
+// Displays the result
 if (isPalindrome)
 {
     Console.WriteLine("The Input is a palindrome");
