df8daec022ba6365ab160bb9bb5c8207b01ac98a
David Xu <david.xufin@gmail.com>
2023-04-26 13:39:49 +0300

    Added acceleration and engine running method

--- DotNet/Object-Oriented/Car.cs
+++ DotNet/Object-Oriented/Car.cs
@@ -1,3 +1,5 @@
+// Coded along with Jani Järvinen
+
 class Car
 {
     // Model,  Make, CurrentSpeed, EngineRunning
@@ -5,8 +7,42 @@ class Car
 
     public string Make { get; set; }
 
-    public int CurrentSpeed { get; }
+    public int CurrentSpeed { get; private set; }
 
-    public bool EngineRunning { get; }
+    public bool EngineRunning { get; private set;}
 
+    //methods
+    public void StartEngine()
+    {
+        EngineRunning = true;
+    }
+
+    public void Accelerate()
+    {
+        
+        if (!EngineRunning)
+        {
+            //return;
+            throw new Exception("Engine not on, please turn on the engine. Use StartEngine()");
+        }
+        
+        const int MaxSpeed = 200;
+        
+        CurrentSpeed += 10;
+
+         if (CurrentSpeed >MaxSpeed)
+        {
+            CurrentSpeed = MaxSpeed;
+        }
+    }
+
+     public void Decelerate()
+    {
+        CurrentSpeed -= 10;
+
+        if (CurrentSpeed < 0)
+        {
+            CurrentSpeed = 0;
+        }
+    }
 }
--- DotNet/Object-Oriented/Program.cs
+++ DotNet/Object-Oriented/Program.cs
@@ -5,8 +5,12 @@ myCar.Make = "Mercedes";
 myCar.Model = "E200";
 //myCar.CurrentSpeed = 55;
 //myCar.EngineRunning = true;
+myCar.StartEngine();
+myCar.Accelerate();
+myCar.Decelerate();
 
 Console.WriteLine("Car Make: "+ myCar.Make);
 Console.WriteLine("Car Model: "+ myCar.Model);
-Console.WriteLine("Current Speed: " +myCar.CurrentSpeed);
+Console.WriteLine("Current Speed: " + myCar.CurrentSpeed);
 Console.WriteLine("Car Engine: "+ myCar.EngineRunning);
+
