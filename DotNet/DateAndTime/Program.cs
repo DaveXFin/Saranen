2337cd4f4825b14452234b91d87cddb70db4fcfa
David Xu <david.xufin@gmail.com>
2023-04-27 13:44:51 +0300

    added functionality to display the days difference and time difference.

---
+++ DotNet/DateAndTime/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Globalization;
+
+
+Console.WriteLine("Welcome to Date and time application");
+
+// Asks user for the first date
+Console.WriteLine("Enter a date and time (mm/dd/yyyy hh:mm:ss)");
+string firstDateInput = Console.ReadLine();
+
+
+// Asks user for the second date
+Console.WriteLine("Enter another date and time (mm/dd/yyyy hh:mm:ss)");
+string secondDateInput = Console.ReadLine();
+
+
+DateTime date1 = DateTime.Parse(firstDateInput);
+DateTime date2 = DateTime.Parse(secondDateInput);
+
+
+Console.WriteLine(date1);
+Console.WriteLine(date2);
+
+TimeSpan diff = date2 - date1;
+Console.WriteLine(diff);
+
+int days = diff.Days;
+int hours = diff.Hours;
+int min = diff.Minutes;
+
+string dateDiff = $"Date difference is {days} day(s)" + $"{hours} hours(s) and {min} minute(s).";
+
+Console.WriteLine(dateDiff);
+
+Console.WriteLine("Total difference in hours: " + diff.TotalHours);
