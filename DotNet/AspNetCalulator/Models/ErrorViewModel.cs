c5281588d392e6fd12ec65a47018e9a796c97f03
David Xu <david.xufin@gmail.com>
2023-05-02 13:10:22 +0300

    Intialized an asp.net application

---
+++ DotNet/AspNetCalulator/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace AspNetCalulator.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
