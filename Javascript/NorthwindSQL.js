b96d63ae17f09caba5bb9a293ec3e68813ca133d
David Xu <david.xufin@gmail.com>
2023-05-08 13:24:48 +0300

    Implemented asking user to select data in console.

--- Javascript/NorthwindSQL.js
+++ Javascript/NorthwindSQL.js
@@ -5,11 +5,44 @@ const sqlite3 = require('sqlite3');
 const db = new sqlite3.Database('../Databases/northwind.db');
 
 
-listFinnishCustomers();
 
-listEployeesInLondon();
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+readline.question(`Please select your choice:
+        1. List Finnish customers
+        2. List London employees
+        3. List Tofu sales.
+        Please enter your choice (1-3): `, choice => {
+    console.log(`You selected choice ${choice}.`);
+    readline.close();
 
-listTofuSales();
+    
+    switch (choice) {
+        case "1":
+            listFinnishCustomers();
+            break
+        case "2":
+            listEmployeesInLondon();
+            break
+        case "3":
+            listTofuSales();
+            break
+    }
+});
+
+// Used to test the functions
+// listFinnishCustomers();
+
+// listEployeesInLondon();
+
+// listTofuSales();
+
+
+
+
+
 
 function listFinnishCustomers() {
     const sql = "SELECT * FROM Customers WHERE Country = 'Finland'";
@@ -22,7 +55,7 @@ function listFinnishCustomers() {
     });
 }
 
-function listEployeesInLondon() {
+function listEmployeesInLondon() {
     const sql = "SELECT * FROM Employees WHERE City = 'London'";
     db.all(sql, (err, rows) => {
         rows.forEach(row => {
