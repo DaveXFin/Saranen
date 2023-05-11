e5fa9211f4e886f5710e0f1a2e5effc6f160fe50
David Xu <david.xufin@gmail.com>
2023-05-11 15:50:38 +0300

    Added CORS to the back end

--- DotNet/NorthwindHttpDemo/Program.cs
+++ DotNet/NorthwindHttpDemo/Program.cs
@@ -1,6 +1,15 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: "MyPolicy",
+                      policy  =>
+                      {
+                          policy.WithOrigins();
+                      });
+});
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -15,6 +24,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors("MyPolicy");
 
 app.UseHttpsRedirection();
 
