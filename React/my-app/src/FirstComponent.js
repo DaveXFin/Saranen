8a94319db5fab997ada26030ac9bad1be4749f7a
David Xu <david.xufin@gmail.com>
2023-05-11 14:48:09 +0300

    Created my first react component called FirstComponent

---
+++ React/my-app/src/FirstComponent.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+
+function FirstComponent(){
+    return(
+        <div>
+            <h1>My first component</h1>
+            <p>Text TextTextTextTextTextText</p>
+        </div>
+    )
+    
+}
+
+
+export default FirstComponent
