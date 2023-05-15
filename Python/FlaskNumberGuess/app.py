071aa689720801a95e1b6bb42248f1dc7916148c
David Xu <david.xufin@gmail.com>
2023-05-15 15:26:04 +0300

    Implemented a number guessing functionaility with flask

--- Python/FlaskNumberGuess/app.py
+++ Python/FlaskNumberGuess/app.py
@@ -1,6 +1,8 @@
-from flask import Flask,Response
-app = Flask(__name__)
+from flask import Flask,Response, request
+import random
 
+app = Flask(__name__)
+answer=0
 
 @app.route('/')
 def hello_world():
@@ -8,5 +10,26 @@ def hello_world():
 
 @app.route("/api/startgame")
 def start_game():
+    answer = random.randint(1,20)
+    print(answer)
     json = '{"status": "Game started"}'
     return Response(json, mimetype='application/json')
+
+@app.route("/api/guess")
+# chekcs the guess answer
+def checkguess():
+    guess =  int(request.args.get('guess', '0'))
+    print(guess)
+
+    statusText = ""
+
+    if (guess == answer):
+        statusText = "Correct! You win the game!"
+    elif (guess < answer):
+        statusText = "The correct number is larger."
+    else:
+        statusText = "The correct number is smaller."
+
+    json = '{ "guess": "' + str(guess) + '", "status": "' + statusText + '" }'
+    return Response(json, mimetype='application/json')
+
